ec39073414e0dec7725be58724b99b5b3ef0ceea
Anteliz <41457612+Anteliz@users.noreply.github.com>
2019-12-06 17:46:44 -0600

    Update pizza.js

--- pizza/lab-jquery-pizza-builder/starter-code/pizza.js
+++ pizza/lab-jquery-pizza-builder/starter-code/pizza.js
@@ -7,97 +7,130 @@ let visiblesauce = 0;
 let visiblegluten = 0;
 let precioInicial = 21;
 
-let pepper = document.querySelectorAll('.cheese');
-$(pepper).css('visibility', 'visible');
+const pepper = document.querySelectorAll('.green-pepper');
+const peperoni = document.querySelectorAll('.pep');
+const mushroom = document.querySelectorAll('.mushroom');
+const gluten = document.querySelector(".crust-gluten-free");
+const sause = document.querySelector(".sauce-white");
+const cheese = document.querySelector('.cheese');
+console.log(cheese);
+
+
+const btpepper = document.querySelector('.btn-green-peppers');
+const btpep = document.querySelector('.btn-pepperonni');
+const btmushroom = document.querySelector('.btn-mushrooms');
+const btsauce = document.querySelector('.btn-sauce');
+const btgluten = document.querySelector('.btn-crust')
+
+const precioPep = document.querySelector('.price li:nth-child(1)');
+const precioMushroom = document.querySelector('.price li:nth-child(2)');
+const precioPepper = document.querySelector('.price li:nth-child(3)');
+const precioSauce = document.querySelector('.price li:nth-child(4)');
+const preciogluten = document.querySelector('.price li:nth-child(5)');
+
 precio(0);
 
-function mostrarOcultarPeppers(){
-    let pepper = document.querySelectorAll('.green-pepper');
+
+btpepper.onclick = function(){
+
     if(visiblepepper === 1){
-        $(pepper).css('visibility', 'visible');
         visiblepepper = 0;
-        $('.btn-green-peppers').addClass('active');
-        document.getElementById('pricepepper').style.display = 'block';
         precio(1);
+        btpepper.setAttribute('class', 'btn btn-green-peppers active');
+        for(let i = 0; i < pepper.length; i++){
+            pepper[i].style.display = 'block';
+        }
+        precioPepper.style.display = 'block';
+
     }
     else{
-        $(pepper).css('visibility', 'hidden');
         visiblepepper = 1;
-        $('.btn-green-peppers').removeClass('active');
-        document.getElementById('pricepepper').style.display = 'none';
         precio(-1);
+        btpepper.setAttribute('class', 'btn btn-green-peppers');
+        for(let i = 0; i < pepper.length; i++){
+            pepper[i].style.display = 'none';
+        }
+        precioPepper.style.display = 'none';
     }
 }
 
-function mostrarOcultarPeperoni(){
-    let peperoni = document.querySelectorAll('.pep');
+btpep.onclick = function(){
+
     if(visiblepeproni === 1){
-        $(peperoni).css('visibility', 'visible');
         visiblepeproni = 0;
-        $('.btn-pepperonni').addClass('active');
-        document.getElementById('pricepep').style.display = 'block';
         precio(1);
+        btpep.setAttribute('class', 'btn btn-pepperonni active');
+        for(let i = 0; i < peperoni.length; i++){
+            peperoni[i].style.display = 'block';
+        }
+        precioPep.style.display = 'block';
     }
     else{
-        $(peperoni).css('visibility', 'hidden');
         visiblepeproni = 1;
-        $('.btn-pepperonni').removeClass('active');
-        document.getElementById('pricepep').style.display = 'none';
         precio(-1);
+        btpep.setAttribute('class', 'btn btn-pepperonni');
+        for(let i = 0; i < peperoni.length; i++){
+            peperoni[i].style.display = 'none';
+        }
+        precioPep.style.display = 'none';
     }
 }
 
-function mostrarOcultarMushroom(){
-    let mushroom = document.querySelectorAll('.mushroom');
+btmushroom.onclick = function(){
+
     if(visiblemushroom === 1){
-        $(mushroom).css('visibility', 'visible');
         visiblemushroom = 0;
-        $('.btn-mushrooms').addClass('active');
-        document.getElementById('pricemushroom').style.display = 'block';
         precio(1);
+        btmushroom.setAttribute('class', 'btn btn-mushrooms active');
+        for(let i = 0; i < mushroom.length; i++){
+            mushroom[i].style.display = 'block';
+        }
+        precioMushroom.style.display = 'block';
     }
     else{
-        $(mushroom).css('visibility', 'hidden');
         visiblemushroom = 1;
-        $('.btn-mushrooms').removeClass('active');
-        document.getElementById('pricemushroom').style.display = 'none';
         precio(-1);
+        btmushroom.setAttribute('class', 'btn btn-mushrooms');
+        for(let i = 0; i < mushroom.length; i++){
+            mushroom[i].style.display = 'none';
+        }
+        precioMushroom.style.display = 'none';
     }
 }
 
-function quitarPonerSauce(){
-    let sause = document.querySelector(".sauce");
+btsauce.onclick = function(){   
+
     if(visiblesauce === 1){
-        $(sause).css('visibility', 'visible');
         visiblesauce = 0;
-        $('.btn-sauce').addClass('active');
-        document.getElementById('pricesauce').style.display = 'block';
         precio(3);
+        btsauce.setAttribute('class', 'btn btn-sauce active');
+        sause.style.display = 'block';
+        precioSauce.style.display = 'block';
     }
     else{
-        $(sause).css('visibility', 'hidden');
         visiblesauce = 1;
-        $('.btn-sauce').removeClass('active');
-        document.getElementById('pricesauce').style.display = 'none';
         precio(-3);
+        btsauce.setAttribute('class', 'btn btn-sauce');
+        sause.style.display = 'none';
+        precioSauce.style.display = 'none';
     }
 }
 
-function conSinGluten(){
-    let gluten = document.querySelector(".crust");
+btgluten.onclick = function(){  
+    
     if(visiblegluten === 1){
-        $(gluten).css('visibility', 'visible');
         visiblegluten = 0;
-        $('.btn-crust').addClass('active');
-        document.getElementById('pricegluten').style.display = 'block';
         precio(5);
+        btgluten.setAttribute('class', 'btn btn-crust active');
+        gluten.style.display = 'block';
+        preciogluten.style.display = 'block';
     }
     else{
-        $(gluten).css('visibility', 'hidden');
         visiblegluten = 1;
-        $('.btn-crust').removeClass('active');
-        document.getElementById('pricegluten').style.display = 'none';
         precio(-5);
+        btgluten.setAttribute('class', 'btn btn-crust');  
+        gluten.style.display = 'none';
+        preciogluten.style.display = 'none';
     }
 }
 
